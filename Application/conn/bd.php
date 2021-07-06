a2b3b1218e7274c3ad35b9c7fd57947fc6bfdfd5
escarllet <65832957+escarllet@users.noreply.github.com>
2021-07-05 20:53:53 -0400

    Update bd.php

--- Application/conn/bd.php
+++ Application/conn/bd.php
@@ -2,9 +2,9 @@
 session_start();
 
 $conn = mysqli_connect(
-            'localhost',
-            'root',
-            '',
+            'misecreto.mysql.database.azure.com',
+            'Escarllet@misecreto',
+            'Hola1234',
             'mi_secreto'
 );
 
